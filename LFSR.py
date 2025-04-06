4c750d4077dcfa336be29dd1a36896ae698ca4c0
AHAdil1 <aadil.bee22seecs@seecs.edu.pk>
2025-04-06 15:58:31 +0500

    Update LFSR.py

--- LFSR.py
+++ LFSR.py
@@ -1,7 +1,7 @@
 # Linear Feedback Shift Register 
 # 4 - Bit LFSR For PRNG
 state = 0b1001
- 
+# Itterating it 20 times
 for i in range(20):
     print("{:04b}".format(state))
     newbit = (state ^ (state >> 1))&1
